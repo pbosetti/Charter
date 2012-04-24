99455358ab3d7e820bf3c35725e1783ffa9aaaa7
Chris Sauerwald <csauerwa@altair.com>
2012-04-24 14:31:25 -0400

    refactor random_client to use lib/charter_client.py

--- python/random_client.py
+++ python/random_client.py
@@ -9,38 +9,8 @@ or just charter_client.py  if the first line in this script works.
 
 import os,sys,string,socket
 import time
-
-class Client:
-    def __init__(self):
-        self.BASE=2000
-        self.id=1
-        self.host='127.0.0.1'
-        self.delay = 0.005
-
-    def array1(self,Array):
-        res='s '
-        for X in Array:
-            res += '  %s'%str(X)
-        self.deliver(res)
-        
-    def clear(self):
-        self.deliver('CLEAR')
-    def close(self):
-        self.deliver('CLOSE')
-    def names(self,Names):
-        self.deliver('NAMES ' + Names)
-    def names(self,Labels):
-        self.deliver('NAMES ' + Labels)
-
-    def deliver(self,Str):
-        Host = socket.gethostbyname(self.host)
-        self.sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM,0)
-        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.sock.connect((Host, self.BASE+self.id))
-        self.sock.send(Str)
-        self.sock.close()
-        time.sleep(self.delay)
-
+sys.path.append('lib/')
+from charter_client import Client
 
 # demo use
 import random
